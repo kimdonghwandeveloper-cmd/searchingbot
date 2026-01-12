001c2382b1f9907460c5aebae29250dbbcb7a8c4
김동환 <kimdonghwandeveloper@gmail.com>
2026-01-12 22:40:25 +0900

    mall commit

--- core-api/src/app/models/mall.py
+++ core-api/src/app/models/mall.py
@@ -3,24 +3,49 @@ from typing import List, Optional
 from pydantic import BaseModel, Field, ConfigDict
 from bson import ObjectId
 
-class PyObjectId(str):
+from typing import Annotated, Any, Callable
+from pydantic import GetJsonSchemaHandler
+from pydantic.json_schema import JsonSchemaValue
+from pydantic_core import core_schema
+
+class _ObjectIdPydanticAnnotation:
     @classmethod
-    def __get_pydantic_core_schema__(cls, _source_type, _handler):
-        from pydantic_json_schema import JsonSchemaValue
-        from pydantic_core import core_schema
+    def __get_pydantic_core_schema__(
+        cls,
+        _source_type: Any,
+        _handler: Callable[[Any], core_schema.CoreSchema],
+    ) -> core_schema.CoreSchema:
+        def validate_from_str(value: str) -> ObjectId:
+            return ObjectId(value)
+
+        from_str_schema = core_schema.chain_schema(
+            [
+                core_schema.str_schema(),
+                core_schema.no_info_plain_validator_function(validate_from_str),
+            ]
+        )
+
         return core_schema.json_or_python_schema(
             json_schema=core_schema.str_schema(),
-            python_schema=core_schema.union_schema([
-                core_schema.is_instance_schema(ObjectId),
-                core_schema.chain_schema([
-                    core_schema.str_schema(),
-                    core_schema.function_plain_schema(ObjectId),
-                ]),
-            ]),
+            python_schema=core_schema.union_schema(
+                [
+                    # check if it's an instance first before doing any further work
+                    core_schema.is_instance_schema(ObjectId),
+                    from_str_schema,
+                ]
+            ),
             serialization=core_schema.plain_serializer_function_ser_schema(
-                lambda x: str(x)
+                lambda instance: str(instance)
             ),
         )
+
+    @classmethod
+    def __get_pydantic_json_schema__(
+        cls, _core_schema: core_schema.CoreSchema, handler: GetJsonSchemaHandler
+    ) -> JsonSchemaValue:
+        return handler(core_schema.str_schema())
+
+PyObjectId = Annotated[ObjectId, _ObjectIdPydanticAnnotation]
 
 class MallConfig(BaseModel):
     id: Optional[PyObjectId] = Field(alias="_id", default=None)
